d47c644da70ca22d0bb67c3ac72f9e01a27167e7
Riteshsa64 <saroderitesh807@gmail.com>
2026-01-04 21:01:33 +0530

    function revision
    function revision

---
+++ fun.py
@@ -0,0 +1,12 @@
+# Simple function to greet a user
+def greet(name):
+    print("Hello", name)
+
+# Function to add two numbers
+def add(a, b):
+    return a + b
+
+# Calling the functions
+greet("Ritesh")
+result = add(5, 3)
+print("Sum is:", result)
